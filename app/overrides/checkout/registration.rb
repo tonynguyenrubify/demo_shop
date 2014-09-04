fe56fafc348091084bf7e16c86383a5bcf7fb62f
tonynguyenrubify <giang@rubify.com>
2014-09-04 16:40:05 +0700

    demo shop with gem spree

---
+++ app/overrides/checkout/registration.rb
@@ -0,0 +1,4 @@
+# Deface::Override.new(:virtual_path  => "spree/checkout/registration",
+#                      :insert_before => "div#registration",
+#                      :text          => "<p>Registration is the future 222 checkout!</p>",
+#                      :name          => "registration_future")
---
+++ app/overrides/home/index.rb
@@ -0,0 +1,4 @@
+# Deface::Override.new(:virtual_path  => "spree/home/index",
+#                      :insert_before  => "div#welcome-message",
+#                      :text          => "Customize home page",
+#                      :name          => "home_future")
